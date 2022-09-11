707db740b713786fd8dd8851c5642b18217d1667
Heather Mosley <hmosley@student.fullsail.edu>
2022-09-11 05:16:51 -0500

    updated routes and updated server

--- api/routes/spotifyRouter.js
+++ api/routes/spotifyRouter.js
@@ -6,49 +6,77 @@ const redirectUri = process.env.REDIRECT_URI
 
 const router = express.Router();
 
+const auth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+const token = `https://accounts.spotify.com/api/token`;
 
+// authorize user login
+router.get('/login', async(req, res, next) => {
+    const state = generateRandomString(16);
+    const scope = 'user-read-private user-read-email';
+
+    res.redirect('https://accounts.spotify.com/authorize?' + 
+    querystring.stringify({
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirectUri,
+        state: state
+    }))
+})
 
 // get user profile
 router.get("/me", async(req, res, next) => {
-    await fetch("https://api.spotify.com/v1/me")
+    await fetch(token, {
+        method: 'GET',
+        headers: {
+            Authorization: `Basic ${auth}`,
+            url: `https://api.spotify.com/v1/me`
+        }
+    })
     .then( response => response.json())
     .then(data => {
         
         res.json(data);
+    })
+    .catch((error) => {
+        res.status(500).json({ message: error.message })
     })
 })
 
 // get albums
 router.get("/me/albums", async(req, res, next) => {
-    await fetch("https://api.spotify.com/v1/me/albums")
+    await fetch(token, {
+        method: "GET",
+        headers: {
+            Authorization: `Basic ${auth}`,
+            url: `https://api.spotify.com/v1/me/albums`
+        }
+    })
     .then( response => response.json())
     .then(data => {
         
         res.json(data);
+    })
+    .catch((error) => {
+        res.status(500).json({ message: error.message })
     })
 })
 
 // get currently playing track
 router.get("/me/player/currently-playing", async(req, res, next) => {
-    await fetch("https://api.spotify.com/v1/me/player/currently-playing")
+    await fetch(token, {
+        method: 'GET',
+        headers: {
+            Authorization: `Basic ${auth}`,
+            url: `https://api.spotify.com/v1/me/player/currently-playing`
+        }
+    })
     .then( response => response.json())
     .then(data => {
         
         res.json(data);
     })
-})
-
-// get all
-router.get('/', async (req, res) => {
-    try {
-        const albums = await Album.find()
-        res.json(albums)
-    } catch(error) {
+    .catch((error) => {
         res.status(500).json({ message: error.message })
-    }
-})
-
-// get one
-router.get('/:id', getAlbum, async (req, res) => {
-    res.json(res.album)
+    })
 })
--- api/server.js
+++ api/server.js
@@ -8,8 +8,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 6000;
 
-const spotifyRouter = require('./routes/spotify');
-const authRouter = require('./routes/auth');
+const spotifyRouter = require('./routes/spotifyRouter');
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -19,8 +18,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log("Database Connection Established"))
 
 app.use(express.json())
-app.use('/api/v1/spotifyRouter', spotifyRouter)
-app.use('/apiv1/authRouter', authRouter)
+app.use('/api/v1', spotifyRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
