1571aba17623f2a1716ebd1fe3d72d9a681cb145
Heather Mosley <hmosley@student.fullsail.edu>
2022-09-18 19:45:13 -0500

    Added model and OAuth

--- api/routes/spotifyRouter.js
+++ api/routes/spotifyRouter.js
@@ -4,10 +4,35 @@ const clientId = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
 const redirectUri = process.env.REDIRECT_URI
 
+const Token = require('../models/tokenModel');
+
 const router = express.Router();
 
-const auth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-const token = `https://accounts.spotify.com/api/token`;
+//const auth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+//const token = `https://accounts.spotify.com/api/token`;
+
+router.get('/refresh_token', function(req, res) {
+
+    const refresh_token = req.query.refresh_token;
+    const token = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64')) },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+  
+    request.post(token, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const access_token = body.access_token;
+        res.send({
+          'access_token': access_token
+        });
+      }
+    });
+  });
 
 // authorize user login
 router.get('/login', async(req, res, next) => {
@@ -23,6 +48,32 @@ router.get('/login', async(req, res, next) => {
         state: state
     }))
 })
+
+router.get('/callback', function(req, res) {
+
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+  
+    if (state === null) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+      const token = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+      };
+    }
+  });
 
 // get user profile
 router.get("/me", async(req, res, next) => {
@@ -80,3 +131,5 @@ router.get("/me/player/currently-playing", async(req, res, next) => {
         res.status(500).json({ message: error.message })
     })
 })
+
+module.exports = router;
